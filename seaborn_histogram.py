57541b746957a911ba1ae68777076c41eceff13a
Buerlike Alai <82178726+buerlikealai@users.noreply.github.com>
2021-04-18 17:38:19 +0200

    Add files via upload
    Using seaborn to plot histogram

---
+++ seaborn_histogram.py
@@ -0,0 +1,20 @@
+import numpy as np
+import seaborn as sns
+
+def sns_his(obs):
+    
+    fig, axes = plt.subplots(1, 1, figsize=(8, 5), sharey=True)
+
+    hist_kws = {'range':(0.3,1)} # depends on the scale of input data
+
+    obs_visual=sns.distplot(obs,bins=20,kde=False,color='lime',hist_kws=hist_kws) # no. of bins depends on the input data
+
+    obs_visual.set_xlabel("predicted probability of pixel=class tree,",fontsize=15)
+
+    obs_visual.set_facecolor("floralwhite")
+    obs_visual.set_alpha(0.2)
+
+    axes.xaxis.set_tick_params(labelsize=15)
+    axes.yaxis.set_tick_params(labelsize=15)
+    
+    return obs_visual
