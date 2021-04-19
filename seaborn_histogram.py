391364d4523181b5ab7ac02591caf07cb3df6dc3
Buerlike Alai <82178726+buerlikealai@users.noreply.github.com>
2021-04-19 15:05:46 +0200

    Update seaborn_histogram.py

--- seaborn_histogram.py
+++ seaborn_histogram.py
@@ -1,15 +1,15 @@
 import numpy as np
 import seaborn as sns
 
-def sns_his(obs):
+def sns_his(obs,data_property):
     
     fig, axes = plt.subplots(1, 1, figsize=(8, 5), sharey=True)
 
     hist_kws = {'range':(0.3,1)} # depends on the scale of input data
 
-    obs_visual=sns.distplot(obs,bins=20,kde=False,color='lime',hist_kws=hist_kws) # no. of bins depends on the input data
+    obs_visual = sns.distplot(obs,bins=20,kde=False,color='lime',hist_kws=hist_kws) # no. of bins depends on the input data
 
-    obs_visual.set_xlabel("predicted probability of pixel=class tree,",fontsize=15)
+    obs_visual.set_xlabel("data_property,",fontsize=15)
 
     obs_visual.set_facecolor("floralwhite")
     obs_visual.set_alpha(0.2)
