9639691feb90ae05ff1e9b825224ed3a8d4e39d0
Buerlike Alai <82178726+buerlikealai@users.noreply.github.com>
2021-04-19 15:03:26 +0200

    Update pd_histogram.py

--- pd_histogram.py
+++ pd_histogram.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pandas as pd
 
-def pd_his(a,b):
-    """ this function is used to plot two arrays with different length"""
+def pd_his(a,b,data_prpperty):
+    """ this function is used to plot two arrays with different length that are a and b,
+    data_prpperty is what the input about"""
 
     # create the dataframe; enumerate is used to make column names
     df = pd.concat(
-                   [pd.DataFrame(a, columns=[f'tree:probability{i}']) 
+                   [pd.DataFrame(a, columns=[f'data_prpperty{i}']) 
                    for i, a in enumerate([a, b], 1)],
                    axis=1
                    )
 
     # plot the data
     ax = df.plot.hist(stacked=True,bins=20, density=True, figsize=(6, 4), range=(0.35,1), fontsize=15, legend=True)
-    ax.set_xlabel("predicted probability of pixel",fontsize=15)
+    ax.set_xlabel("data_prpperty",fontsize=15)
     
     return ax
